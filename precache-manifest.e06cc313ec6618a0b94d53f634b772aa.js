bb9cad65942dff43970db7482d511d8e975254a3
matt3188 <matt3188@users.noreply.github.com>
2022-02-01 18:48:56 +0000

    Deploying to gh-pages from @ matt3188/product-lifecycle-manager@43e599b1e8b02a9ec34f28daae3947063838b5c5 🚀

---
+++ precache-manifest.e06cc313ec6618a0b94d53f634b772aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea704f41b9f804eecc4",
+    "url": "/product-lifecycle-manager/css/app.a5c1d9ba.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/product-lifecycle-manager/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d8cda9aabda5ef8dd90a139cce747dcf",
+    "url": "/product-lifecycle-manager/index.html"
+  },
+  {
+    "revision": "25a5da9d654afc55ff5c",
+    "url": "/product-lifecycle-manager/js/about.41effba9.js"
+  },
+  {
+    "revision": "cea704f41b9f804eecc4",
+    "url": "/product-lifecycle-manager/js/app.189627ea.js"
+  },
+  {
+    "revision": "8e038416d2f386680d35",
+    "url": "/product-lifecycle-manager/js/chunk-vendors.fbf3d271.js"
+  },
+  {
+    "revision": "9748f5e0314f570359fe12043cee6da3",
+    "url": "/product-lifecycle-manager/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/product-lifecycle-manager/robots.txt"
+  }
+]);
